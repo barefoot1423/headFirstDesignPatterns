b6f245f79bda7828e962b9c5a74e4f2f58470c8d
Hyunwoo Cho <barefoot1423@gmail.com>
2019-12-14 18:02:01 -0500

    first example if it happens in local only

--- src/com/hyunwoo/chapter11proxy/GumballMachine.java
+++ src/com/hyunwoo/chapter11proxy/GumballMachine.java
@@ -7,8 +7,16 @@ public class GumballMachine {
     @Getter
     String location;
 
+    @Getter
+    int count;
+
+    @Getter
+    String state;
+
     public GumballMachine(String location, int count) {
+        this.state = "wating for quarter";
         this.location = location;
+        this.count = count;
     }
 
 }
